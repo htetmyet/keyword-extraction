81d00346f1a465b834f235fd40bed58d7db89b1c
Htet Myet Lynn <htetmyet@gmail.com>
2016-02-13 17:02:28 +0900

    Updated for distributing feature vectors
    updated to contribute features for unigrams and bigrams.

--- key_extract_new.py
+++ key_extract_new.py
@@ -17,7 +17,7 @@ def convert_to_string(text):
     return get_string
 
 def remov_stopword(text):
-    stopwords = open ('smartstoplist.txt', 'r').read().splitlines()
+    stopwords = open ('nothesmartstoplist.txt', 'r').read().splitlines()
     text = ' '.join([word for word in text.split() if word not in stopwords])
     return text
 
@@ -49,7 +49,7 @@ def term_frequency(w_tf):
     return tf_score
 
 def count_total_corpus():
-    tot_corpus =  len(glob.glob1("dataset","00*.txt"))
+    tot_corpus =  len(glob.glob1("dataset","doc*.txt"))
     return tot_corpus
 
 def count_nterm_doc(word):
@@ -57,7 +57,7 @@ def count_nterm_doc(word):
     get_total = count_total_corpus()
     while (get_total>0):
         n_files = str(get_total)
-        get_doc = open('dataset/00'+n_files+'.txt', 'r')
+        get_doc = open('dataset/doc'+n_files+'.txt', 'r')
         raw_doc = get_doc.read()
         if word in raw_doc:
            num_count += 1
@@ -88,6 +88,21 @@ def involve_in_title(word, get_title):
     else:
         result_this = 0
     return result_this   
+
+def get_bi_tfidf(bi_dict, bigrams):
+    i=0
+    for bigrams[i] in bigrams:
+        for w in bigrams[i]:
+            if w in bi_dict:
+                print bi_dict[w]
+                
+def get_val_bipairs(bi_dict, bigrams):
+    val_pairs = [(bi_dict[x]+bi_dict[y]) for x,y in bigrams]
+    return val_pairs
+
+def get_val_tripairs(tri_dict, trigrams):
+    val_pairs = [(tri_dict[x]+tri_dict[y]+tri_dict[z]) for x,y,z in trigrams]
+    return val_pairs
 
 def main():
     ##set_file_name = raw_input('Enter a file name: ')
@@ -122,7 +137,8 @@ def main():
     get_nouns = [[Word(*x) for x in sent] for sent in pos_tag]
 
     #NNP Rules
-    rule_0 = W(pos = "NN") | W(pos = "NNP")
+    rule_0 = W(pos = "NNS")| W(pos = "NNS")| W(pos = "NN") | W(pos = "NNP")
+    rule_05 = W(pos = "NNP") + W(pos = "NNS")
     rule_1 = W(pos = "WP$") + W(pos = "NNS")
     rule_2 = W(pos = "CD") + W(pos = "NNS")
     rule_3 = W(pos = "NN") + W(pos = "NN")
@@ -139,6 +155,7 @@ def main():
     rule_14 = W(pos = "JJ") + W(pos = "NNS")
     rule_15 = W(pos = "PRP$") + W(pos = "NNS")
     rule_16 = W(pos = "PRP$") + W(pos = "NN")
+    rule_02 = W(pos = "NN") + W(pos = "NN") + W(pos = "NN")
     rule_17 = W(pos = "NN") + W(pos = "NNS") + W(pos = "NN")
     rule_18 = W(pos = "NNP") + W(pos = "NNP") + W(pos = "NNP")
     rule_19 = W(pos = "JJ") + W(pos = "NN") + W(pos = "NNS")
@@ -154,8 +171,8 @@ def main():
     rule_29 = W(pos = "DT") + W(pos = "NNP") + W(pos = "NNP") + W(pos = "NNP")
     rule_30 = W(pos = "DT") + W(pos = "NNP") + W(pos = "NN") + W(pos = "NN") 
 
-    NP_bi_gram_set = (rule_1)|(rule_2)|(rule_3)|(rule_4)|(rule_5)|(rule_6)|(rule_7)|(rule_8)|(rule_9)|(rule_10)|(rule_11)|(rule_12)|(rule_13)|(rule_14)|(rule_15)|(rule_16)
-    NP_tri_gram_set = (rule_17)|(rule_18)|(rule_19)|(rule_20)|(rule_21)|(rule_22)|(rule_23)|(rule_24)|(rule_25)|(rule_26)|(rule_27)|(rule_28)
+    NP_bi_gram_set = (rule_05)|(rule_1)|(rule_2)|(rule_3)|(rule_4)|(rule_5)|(rule_6)|(rule_7)|(rule_8)|(rule_9)|(rule_10)|(rule_11)|(rule_12)|(rule_13)|(rule_14)|(rule_15)|(rule_16)
+    NP_tri_gram_set = (rule_02)|(rule_17)|(rule_18)|(rule_19)|(rule_20)|(rule_21)|(rule_22)|(rule_23)|(rule_24)|(rule_25)|(rule_26)|(rule_27)|(rule_28)
     NP_quard_gram_set = (rule_29)|(rule_30)
 
     #Rule set function
@@ -169,22 +186,295 @@ def main():
     bag_of_triNP = []
     bag_of_fourNP = []
     total__tfidf = 0
-    ############GET UNIGRAMS############
+    ###################################GET UNIGRAMS###################################
     print "UNIGRAM -->"
     for k, s in enumerate(get_nouns):
         for match in finditer(get_uni_gram, s):
             x, y = match.span() #the match spans x to y inside the sentence s
             ##print pos_tag[k][x:y]
             bag_of_NP += pos_tag[k][x:y]
-            
+
+    ###############      
     #Term Frequency for unigrams    
     print "\nTerm Frequency for each:"
+    total__tfidf = 0
+    uni_tfidf_values = ''
+    str_uni_grams = ''
     total_docs = count_total_corpus()
     fdist = nltk.FreqDist(bag_of_NP)
+    
+    ##STORE UNIGRAMS
+    unzipped_uni = zip(*bag_of_NP)
+    str_unigrams = list(unzipped_uni[0])
+    ##get_unigrams = zip(str_unigrams,str_unigrams[1:])[::2]
+    ###############
     for word in fdist:
         fq_word = fdist[word]
-        print '%s->%d' % (word, fq_word)
+        ##print '%s->%d' % (word, fq_word)
         get_tf = term_frequency(fq_word)
+
+        ### FEATURES ###
+        ##Tuple to String##
+        to_string = ':'.join(word)
+        get_this_string = convert_to_string(to_string)
+
+        ##DF Score
+        num_of_doc_word = count_nterm_doc(get_this_string)
+        ##
+        ##TF.IDF Score
+        idf_score = inverse_df(total_docs, num_of_doc_word)
+        tf_idf_scr = get_tf * idf_score
+        total__tfidf += tf_idf_scr
+
+        ##GET EACH UNIGRAMS TFIDF
+        uni_tfidf_scr = repr(tf_idf_scr)+' '
+        uni_tfidf_values += uni_tfidf_scr
+        str_uni_grams += get_this_string+','
+
+    ##BUILD DICT FOR EACH TERMS
+    get_uni_float = [float(x) for x in uni_tfidf_values.split()]
+    get_uni_list = str_uni_grams.split(',')
+    unigram_dict = dict(zip(get_uni_list, get_uni_float))
+    ###########################  
+
+    ##GET TFIDF FOR UNIGRAMS##
+    ############
+    uni_avg_tfidf = (sum(map(float,get_uni_float)))/(len(get_uni_float))
+    ###########################
+    get_zip_str = [''.join(item) for item in str_unigrams]
+    ###Unigrams string with TFIDF###
+    unigrams_list =  zip(get_zip_str, get_uni_float)
+    ###########################
+    
+    print '===============***==============='
+    print 'Total Unigrams: ', len(fdist)
+    print 'Total tfidf', total__tfidf
+    print 'Average TF.IDF: ', uni_avg_tfidf
+    print '===============***==============='
+    ###########################
+    ##### TFIDF FEATURE MATRIX #####
+    uni_feat_tfidf = []
+    for x in unigrams_list:
+        if float(x[1]) > uni_avg_tfidf:
+            uni_feat_tfidf.append(1)
+        else:
+            uni_feat_tfidf.append(0)
+    zip_tfidf_feat = zip(get_zip_str, get_uni_float, uni_feat_tfidf)
+    ################################
+    ##### First Sentence Feat #####
+    uni_fir_sen = []
+    for x in unigrams_list:
+        get_res = chk_frs_sen(x[0], file_name)
+        if get_res == 1:
+            uni_fir_sen.append(1)
+        else:
+            uni_fir_sen.append(0)
+    zip_fir_sen_feat = zip(get_zip_str, get_uni_float, uni_feat_tfidf, uni_fir_sen)
+    ################################
+    ##### Involve in Title #####
+    uni_title_feat = []
+    for x in unigrams_list:
+        get_res = involve_in_title(x[0], title)
+        if get_res == 1:
+            uni_title_feat.append(1)
+        else:
+            uni_title_feat.append(0)
+    zip_uni_feats = zip(get_zip_str, get_uni_float, uni_feat_tfidf, uni_fir_sen, uni_title_feat)
+    print zip_uni_feats
+    ################################
+    print "\n\n"
+    ###################################GET BIGRAMS###################################
+    print "BIGRAM -->"
+    for k, s in enumerate(get_nouns):
+        for match in finditer(get_bi_gram, s):
+            x, y = match.span()
+            ##print pos_tag[k][x:y]
+            bag_of_biNP += pos_tag[k][x:y]
+            
+    ##Term Frequency for bigrams##
+    total__tfidf = 0
+    bi_tfidf_values = ''
+    str_bi_grams = ''
+    ###############
+    
+    ##STORE BIGRAMS
+    unzipped = zip(*bag_of_biNP)
+    str_bigrams = list(unzipped[0])
+    get_bigrams = zip(str_bigrams,str_bigrams[1:])[::2]
+    
+    ###############
+    
+    print "\nTerm Frequency for bi:"
+    bi_dist = nltk.FreqDist(bag_of_biNP)
+    for word in bi_dist:
+        
+        tq_word = bi_dist[word]
+        ##print '%s-->%d' % (word, tq_word)
+        get_tf = term_frequency(tq_word)
+        
+        ### FEATURES ###
+        ##Tuple to String##
+        to_string = ':'.join(word)
+        get_this_string = convert_to_string(to_string)
+        
+        ##DF Score
+        num_of_doc_word = count_nterm_doc(get_this_string)
+        
+        ##TF.IDF Score
+        idf_score = inverse_df(total_docs, num_of_doc_word)
+        tf_idf_scr = get_tf*idf_score
+        total__tfidf += tf_idf_scr
+
+        ##GET EACH BIGRAMS TFIDF
+        get_tfidf_scr = repr(tf_idf_scr)+' '
+        bi_tfidf_values += get_tfidf_scr
+        str_bi_grams += get_this_string+','
+
+    ##BUILD DICT FOR EACH TERMS
+    get_float = [float(x) for x in bi_tfidf_values.split()]
+    get_bi_list = str_bi_grams.split(',')
+    bigram_dict = dict(zip(get_bi_list, get_float))
+    ###########################
+    
+    ##GET TFIDF FOR BIGRAMS##
+    get_bi_floats = get_val_bipairs(bigram_dict, get_bigrams)
+    get_zip = dict(zip(get_bigrams, get_bi_floats))
+    ############
+    real_avg_tfidf = (sum(map(float,get_bi_floats)))/(len(get_bi_floats))
+    ###########################
+    get_zip_str = [' '.join(item) for item in get_bigrams]
+    ###Bigrams string with TFIDF###
+    bigrams_list =  zip(get_zip_str, get_bi_floats)
+    ###########################
+    ##print bigrams_list
+    
+    print '===============***==============='
+    print 'Total Bigrams: ', len(get_bi_floats)
+    print 'total tfidf: ', sum(map(float,get_bi_floats))
+    print 'Average TF.IDF: ', real_avg_tfidf
+    print '===============***==============='
+    ##print len(bi_str2_float(bi_tfidf_values))
+    ##print type(bag_of_biNP)
+    ##### TFIDF FEATURE MATRIX #####
+    feat_tfidf_matx = []
+    for x in bigrams_list:
+        if float(x[1]) > real_avg_tfidf:
+            feat_tfidf_matx.append(1)
+        else:
+            feat_tfidf_matx.append(0)
+            
+    tfidf_feat = zip(get_zip_str, get_bi_floats, feat_tfidf_matx)
+    #################################
+    #### FIRST SENTENCE FEATURE ####
+    feat_fir_sen = []
+    for x in tfidf_feat:
+        get_res = chk_frs_sen(x[0], file_name)
+        if get_res == 1:
+            feat_fir_sen.append(1)
+        else:
+            feat_fir_sen.append(0)
+            
+    fir_sen_feat = zip (get_zip_str, get_bi_floats, feat_tfidf_matx, feat_fir_sen)
+    ##print fir_sen_feat
+    #################################
+    #### INVOLVE IN TITLE FEATURE ###
+    feat_invol_tit = []
+    for x in fir_sen_feat:
+        get_res = involve_in_title(x[0], title)
+        if get_res == 1:
+            feat_invol_tit.append(1)
+        else:
+            feat_invol_tit.append(0)
+    invol_tit_feat = zip (get_zip_str, get_bi_floats, feat_tfidf_matx, feat_fir_sen, feat_invol_tit)
+    print invol_tit_feat
+    #################################
+    print "\n\n"
+    
+    ###################################GET TRIGRAMS###################################
+    print "TRIGRAM -->"
+    for k, s in enumerate(get_nouns):
+        for match in finditer(get_tri_gram, s):
+            x, y = match.span()
+            print pos_tag[k][x:y]
+            bag_of_triNP += pos_tag[k][x:y]
+            
+    #Term Frequency for trigrams
+    total__tfidf = 0
+    tri_tfidf_values = ''
+    str_tri_grams = ''
+    ###############
+    
+    ##STORE BIGRAMS
+    unzipped_tri = zip(*bag_of_triNP)
+    str_trigrams = list(unzipped_tri[0])
+    get_trigrams = zip(str_trigrams,str_trigrams[1:],str_trigrams[2:3])[::3]
+    print get_trigrams
+    ###############
+    
+    print "\nTerm Frequency for tri:"
+    tri_dist = nltk.FreqDist(bag_of_triNP)
+    for word in tri_dist:
+        tr_fq = tri_dist[word]
+        ##print '%s-->%d' % (word, tr_fq)
+        get_tf = term_frequency(tr_fq)
+    
+        ### FEATURES ###
+        ##Tuple to String##
+        to_string = ':'.join(word)
+        get_this_string = convert_to_string(to_string)
+        ##DF Score
+        num_of_doc_word = count_nterm_doc(get_this_string)
+        ##
+        ##TF.IDF Score
+        idf_score = inverse_df(total_docs, num_of_doc_word)
+        tf_idf_scr = get_tf * idf_score
+        total__tfidf += tf_idf_scr
+
+        ##GET EACH TRIGRAMS TFIDF
+        get_tfidf_scr = repr(tf_idf_scr)+' '
+        tri_tfidf_values += get_tfidf_scr
+        str_tri_grams += get_this_string+','
+
+    ##BUILD DICT FOR EACH TERMS
+    get_tri_float = [float(x) for x in tri_tfidf_values.split()]
+    get_tri_list = str_tri_grams.split(',')
+    trigram_dict = dict(zip(get_tri_list, get_tri_float))
+    ###########################
+    
+    ##GET TFIDF FOR TRIGRAMS##
+    get_tri_floats = get_val_tripairs(trigram_dict, get_trigrams)
+    get_tri_zip = dict(zip(get_trigrams, get_tri_floats))
+    print get_tri_zip
+    ############
+    real_avg_tfidf = (sum(map(float,get_bi_floats)))/(len(get_bi_floats))
+    ###########################
+    get_zip_str = [' '.join(item) for item in get_bigrams]
+    ###Bigrams string with TFIDF###
+    bigrams_list =  zip(get_zip_str, get_bi_floats)
+    ###########################
+    print '===============***==============='
+    print 'Total Trigrams: ', len(tri_dist)
+    print 'Total tfidf', total__tfidf
+    print 'Real avg TF.IDF: '
+    print '===============***==============='
+    print "\n\n"
+
+    ###################################GET 4-GRAMS###################################
+    print "4th GRAM -->"
+    for k, s in enumerate(get_nouns):
+        for match in finditer(get_quard_gram, s):
+            x,y = match.span()
+            print pos_tag[k][x:y]
+            bag_of_fourNP += pos_tag[k][x:y]
+    
+    #Term Frequency for 4-grams
+    total__tfidf = 0
+    print "\nTerm Frequency for 4:"
+    f_dist = nltk.FreqDist(bag_of_fourNP)
+    for word in f_dist:
+        fr_fq = f_dist[word]
+        print '%s-->%d' % (word, fr_fq)
+        get_tf = term_frequency(fr_fq)
 
         ### FEATURES ###
         ##Tuple to String##
@@ -204,73 +494,23 @@ def main():
         ##Involve in Title
         in_title = involve_in_title(get_this_string, title)
 
+        ##tfidf_values += tf_idf_scr
         print 'TF.IDF: ', tf_idf_scr
         print 'In 1st sentence: ', first_sen
         print 'In Title: ', in_title
         print '\n'
-      
-        
-    print '===============***==============='
-    print 'Total Unigrams: ', len(fdist)
-    print 'Average TF.IDF: ', total__tfidf/len(fdist)
-    print '===============***==============='
-    print "\n\n"
-
-    ############GET BIGRAMS############
-    print "BIGRAM -->"
-    for k, s in enumerate(get_nouns):
-        for match in finditer(get_bi_gram, s):
-            x, y = match.span()
-            print pos_tag[k][x:y]
-            bag_of_biNP += pos_tag[k][x:y]
-    ##bigrams = Counter(zip(bag_of_biNP,bag_of_biNP[1:]))
-    ##print(bigrams)
-    #Term Frequency for bigrams
-    print "\nTerm Frequency for bi:"
-    bi_dist = nltk.FreqDist(bag_of_biNP)
-    for word in bi_dist:
-        print '%s-->%d' % (word, bi_dist[word])
-    print '===============***==============='
-    print 'Total Bigrams: ', len(bi_dist)
-    print 'Total term frequency: ', len(bag_of_biNP)
-    print '===============***==============='    
-    print "\n\n"
-
-    ############GET TRIGRAMS############
-    print "TRIGRAM -->"
-    for k, s in enumerate(get_nouns):
-        for match in finditer(get_tri_gram, s):
-            x, y = match.span()
-            print pos_tag[k][x:y]
-            bag_of_triNP += pos_tag[k][x:y]
-    #Term Frequency for trigrams
-    print "\nTerm Frequency for tri:"
-    tri_dist = nltk.FreqDist(bag_of_triNP)
-    for word in tri_dist:
-        print '%s-->%d' % (word, tri_dist[word])
-    print '===============***==============='
-    print 'Total Trigrams: ', len(tri_dist)
-    print 'Total term frequency: ', len(bag_of_triNP)
-    print '===============***==============='
-    print "\n\n"
-
-    ############GET 4-GRAMS############
-    print "4th GRAM -->"
-    for k, s in enumerate(get_nouns):
-        for match in finditer(get_quard_gram, s):
-            x,y = match.span()
-            print pos_tag[k][x:y]
-            bag_of_fourNP += pos_tag[k][x:y]
-    #Term Frequency for 4-grams
-    print "\nTerm Frequency for 4:"
-    f_dist = nltk.FreqDist(bag_of_fourNP)
-    for word in f_dist:
-        print '%s-->%d' % (word, f_dist[word])
     print '===============***==============='
     print 'Total 4-grams: ', len(f_dist)
-    print 'Total term frequency: ', len(bag_of_fourNP)
+    print 'Total tfidf', total__tfidf
+    try:
+        avg_tfidf = total__tfidf/(len(f_dist))
+    except ZeroDivisionError as e:
+        print 'zero result.'
+    print 'Average TF.IDF: ', avg_tfidf
+    print 'Real avg TF.IDF: ', avg_tfidf*4
     print '===============***==============='
     print "\n\n"
+    
 
 if __name__ == '__main__':
     main()
